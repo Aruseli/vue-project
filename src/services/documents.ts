5cd1ffa3e5aaee6a5dbe904f62af643540bf1575
Aruseli <llingveri@gmail.com>
2024-02-21 22:35:59 +0300

    https://gitlab-deep.pletnev.biz/showcase/code/kiosk/-/issues/33

--- src/services/documents.ts
+++ src/services/documents.ts
@@ -1,22 +1,54 @@
-let lastCounterResetDate = new Date().setHours(0, 0, 0, 0); let counter = 0;
+const SET_COUNTER_IN_LS = 'SET_COUNTER';
+
+function setCounterToLocalStorage(key: string, count: number) {
+  localStorage.setItem(key, JSON.stringify({ counter: count }));
+}
+
+let lastCounterResetDate = new Date().setHours(0, 0, 0, 0);
+let counter = 0;
 function resetCounterIfNewDay() {
   const today = new Date().setHours(0, 0, 0, 0);
 
   if (today > lastCounterResetDate) {
-    counter = 0;
+    counter = 1;
     lastCounterResetDate = today;
+  }
+  setCounterToLocalStorage('LAST_RESET', counter);
+}
+
+function getStoredCounterFromLs(key: string) {
+  try {
+    const storedData = localStorage.getItem(key);
+
+    if (!storedData) return null;
+    return JSON.parse(storedData).counter || 1;
+  } catch (error) {
+    console.warn(`Error retrieving ${key}:`, error);
+    return null;
   }
 }
 
 export function getNextInventoryNumber() {
+  const storedCount = getStoredCounterFromLs('LAST_RESET');
   resetCounterIfNewDay();
-  counter += 1;
-  return counter.toString().padStart(4, "0");
+  const newCount = storedCount ? storedCount + 1 : 0;
+  setCounterToLocalStorage('LAST_RESET', newCount);
+  return newCount.toString().padStart(4, "0");
+
+  // resetCounterIfNewDay();
+  // counter += 1;
+  // return counter.toString().padStart(4, "0");
 }
 
 export function getNextInvoiceNumber() {
+  const storedCount = getStoredCounterFromLs('LAST_RESET');
   resetCounterIfNewDay();
-  counter += 1;
-  return counter.toString().padStart(4, "0");
+  const newCount = storedCount ? storedCount + 1 : 0;
+  setCounterToLocalStorage('LAST_RESET', newCount);
+  return newCount.toString().padStart(4, "0");
+
+  // resetCounterIfNewDay();
+  // counter += 1;
+  // return counter.toString().padStart(4, "0");
 }
 
