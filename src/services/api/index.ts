0082e965b715d457686a36aa066dfc97639bad71
FreePhoenix888 <freephoenix888@gmail.com>
2024-03-26 20:53:31 +0500

    Return `response` instead of `response.data` from `apiReportsGetView`

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -146,7 +146,7 @@ export async function apiGetCorrespondentByEntity(entityId: string, corrType: st
 export async function apiReportsGetView(viewId: string, parameters?: Record<string, any> | Array<Record<string,any>>) {
   const response = await fetchApi('/api/v2/reports/getView', { view_id: viewId, parameters },'text')
   console.log({response})
-  return response.data
+  return response
 }
 
 export type ApiGoodCategory = {
