3f1918b98a951918f0b3370b6e56b7449313528b
FreePhoenix888 <freephoenix888@gmail.com>
2024-03-28 13:00:08 +0500

    Add `printDocument` doc, fix `printLeftovers` doc

--- src/services/print.ts
+++ src/services/print.ts
@@ -3,6 +3,18 @@ import { QVueGlobals } from "quasar";
 import { apiReportsGetView } from "./api";
 import { wsSendMessage } from "./local-device-ws";
 
+/**
+ * Prints document
+ * 
+ * @example
+ * ```
+ * <IconButton
+ *   round
+ *   :icon="print"
+ *   @click="printDocument({documentId: '7d92dd13-a34c-4b04-bc3c-fba23dbc7293', $q})"
+ * />
+ * ```
+ */
 export async function printDocument({documentId, $q, viewId, langCode = i18next.language}: {documentId: number, $q: QVueGlobals, viewId: string, langCode: string}) {
   console.log({documentId})
   $q.loading.show();
@@ -40,11 +52,11 @@ export async function printDocument({documentId, $q, viewId, langCode = i18next.
  * 
  * @example
  * ```
-<IconButton
-  round
-  :icon="print"
-  @click="printLeftovers({$q, dateTo: '21.03.2024', shouldShowZeroRemains: true, stockList: ['1362c8b4-3642-408b-9fd0-057acf547c60']})"
-/>
+ * <IconButton
+ *   round
+ *   :icon="print"
+ *   @click="printLeftovers({$q, dateTo: '21.03.2024', shouldShowZeroRemains: true, stockList: ['1362c8b4-3642-408b-9fd0-057acf547c60']})"
+ * />
  * ```
  */
 export async function printLeftovers({$q, viewId = '3b3ed231-f11c-46d9-ba66-253a05940968', dateTo, shouldShowZeroRemains, stockList, langCode = i18next.language}: {$q: QVueGlobals, viewId?: string, stockList: Array<string>, dateTo: string, shouldShowZeroRemains: boolean, langCode?: string}) {
