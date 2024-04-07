8e3e05128684574abaf85316443a58629c6e514e
Aruseli <llingveri@gmail.com>
2024-04-07 20:44:36 +0300

    https://gitlab-deep.pletnev.biz/showcase/code/kiosk/-/issues/30

--- src/components/dialog/redirect-dialog.vue
+++ src/components/dialog/redirect-dialog.vue
@@ -22,7 +22,7 @@ const props = defineProps({
 <template>
   <Modal :isOpen="props.modelValue" to="#redirect-dialog">
     <div class="card_container">
-      <q-card class="q-pa-sm">
+      <q-card class="q-pa-sm fit card_style">
         <q-card-section class="row items-center justify-end" />
         <q-card-section class="column items-center q-mb-xs-sm">
           <div class="text-h3 q-mb-sm-sm q-mb-xs-xs text-center title_style">{{ $t(props.title) }}</div>
@@ -40,12 +40,15 @@ const props = defineProps({
 
 <style>
 .card_container {
-  width: 70%;
-  max-width: 80vw;
+  width: 45vw;
   height: max-content;
   @media (max-width: 899px) {
     width: 90vw;
   }
+}
+
+.card_style {
+  border-radius: 2rem;
 }
 .title_style {
   text-transform: none;
