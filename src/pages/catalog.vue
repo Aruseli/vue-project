f014ffae8a94f0755ea46c67ad218c8565bfe010
Aruseli <llingveri@gmail.com>
2024-04-11 03:23:09 +0300

    https://gitlab-deep.pletnev.biz/showcase/code/kiosk/-/issues/62

--- src/pages/catalog.vue
+++ src/pages/catalog.vue
@@ -1,6 +1,7 @@
-<script setup>
+<script setup lang="ts">
 import CatalogHeader from '../components/catalog/catalog-header.vue';
 import CatalogNew from '../components/catalog/catalog-new.vue';
+import CatalogV3 from '../components/components-v3/catalog-v3.vue';
 import Catalog from '../components/catalog/catalog.vue';
 import { useAppStore } from 'src/stores/app';
 import CartDrawer from '../components/catalog/cart/cart-drawer.vue';
@@ -12,8 +13,9 @@ import CartDrawer from '../components/catalog/cart/cart-drawer.vue';
 <template>
   <q-page :style="{ direction: app.lang_dir }" class="flex align-center relative justify-start window-full">
     <CartDrawer />
-    <CatalogHeader />
-    <CatalogNew v-if="app.kioskState.settings?.alt_ui" />
+    <CatalogHeader v-if="app.kioskState.settings?.alt_ui != 'design_v3'"/>
+    <CatalogNew v-if="app.kioskState.settings?.alt_ui == 'design_v2'" />
+    <CatalogV3 v-if="app.kioskState.settings?.alt_ui == 'design_v3'" />
     <Catalog v-else />
   </q-page>
 </template>
