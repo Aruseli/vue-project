e9858c77c35fb7d97f02d57e6a770aa1c5b63061
Aruseli <llingveri@gmail.com>
2024-04-16 21:58:13 +0300

    https://gitlab-deep.pletnev.biz/showcase/code/kiosk/-/issues/64

--- src/components/components-v3/cart/cart-drawer.vue
+++ src/components/components-v3/cart/cart-drawer.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
   import { evaMinusOutline, evaPlusOutline } from '@quasar/extras/eva-icons';
-import { t } from 'i18next';
-import { useQuasar } from 'quasar';
-import { ref } from 'vue';
-import { useRouter } from 'vue-router';
-import { useAppStore } from '../../../stores/app';
-import { useCartStore } from '../../../stores/cart';
-import IconButton from '../../buttons/icon-button.vue';
-import RectangularButton from '../../buttons/rectangular-button.vue';
-import OrderCheck from './order-check.vue';
-import DividerThin from '../../dividers/divider-thin.vue';
+  import { t } from 'i18next';
+  import { useQuasar } from 'quasar';
+  import { ref } from 'vue';
+  import { useRouter } from 'vue-router';
+  import { useAppStore } from '../../../stores/app';
+  import { useCartStore } from '../../../stores/cart';
+  import IconButton from '../../buttons/icon-button.vue';
+  import RectangularButton from '../../buttons/rectangular-button.vue';
+  import OrderCheck from './order-check.vue';
+  import DividerThin from '../../dividers/divider-thin.vue';
 
   const $q = useQuasar();
   const router = useRouter();
@@ -23,6 +23,10 @@ import DividerThin from '../../dividers/divider-thin.vue';
       required: true,
       default:false,
     },
+    additionalCartClass: {
+      type: String,
+      default: ''
+    }
   })
 
   const closeDrawerCart = () => {
@@ -30,7 +34,7 @@ import DividerThin from '../../dividers/divider-thin.vue';
   }
 
   const isDisabled = ref(false);
-
+//app.kioskState.settings?.customer_successful_order_notify_duration_ms ??
   async function submitOrder() {
     // кнопка будет недоступна для повторного клика
     isDisabled.value = true;
@@ -42,7 +46,7 @@ import DividerThin from '../../dividers/divider-thin.vue';
         closeDrawerCart();
         cartStore.clearCart();
         router.push('hello');
-      }, app.kioskState.settings?.customer_successful_order_notify_duration_ms ?? 7000);
+      },  700000);
     } catch (err) {
       console.error('ordersStore.selectOrder error:', err)
       $q.notify({
@@ -62,176 +66,176 @@ import DividerThin from '../../dividers/divider-thin.vue';
 
 <template>
   <transition name="drawer_animation">
-    <div class="drawer_style_v3 bg-grey-3" v-if="props.isOpen">
-      <div class="full-height q-pa-xl">
+    <div class="drawer_style_v3 bg-grey-3" :class="props.additionalCartClass" v-if="props.isOpen">
+      <transition mode="out-in">
+        <div v-if="app.orderDialog == false" class="full-height">
+          <div class="full-height q-pa-xl">
 
-        <div class="q-mb-lg-md q-mb-xs-sm">
-          <div class="row items-center justify-between q-mb-lg-md q-mb-xs-xs">
-            <div class="text-h2 text-left text-white text-uppercase col-10  text-weight-bold">
-              {{ $t('order') }}
-            </div>
-
-            <!-- close + bin -->
-            <div class="row absolute cart_buttons">
-              <q-btn flat round @click="cartStore.clearCart()" class="q-mr-sm">
-                <q-icon name="img:/bin.svg" size="2.5rem" class="text-grey" />
-              </q-btn>
-              <IconButton
-                icon="close"
-                textColor="grey"
-                :rounded="false"
-                :round="true"
-                :flat="true"
-                size="xl"
-                class="cart_close_button"
-                @click="closeDrawerCart"
-              />
-            </div>
-          </div>
-          <div class="bg-negative full-width" style="height: 0.1rem" />
-        </div>
-
-        <div v-if="!cartStore.cart.length" class="q-pa-lg-md q-pa-xs-sm text-center">
-          <div class="text-h2">{{ $t('empty_cart') }}</div>
-        </div>
-
-        <q-scroll-area class="fit">
-          <div class="row container_settings">
-
-            <!-- flex row container -->
-            <div
-              v-for="(item, index) in cartStore.cartExtended" :key="index"
-              class="cart_product_item_v3 bg-grey-1 row pa-20"
-            >
-
-              <!-- image -->
-              <div class="col-3 q-mr-xl">
-                <q-img
-                  :src="item.image"
-                  :ration="1"
-                  height="150px"
-                  fit="none"
-                  class="img_style_v3"
-                >
-                  <template #loading>
-                    <div class="text-subtitle1 text-black">
-                      Loading...
-                    </div>
-                  </template>
-                </q-img>
-              </div>
-
-              <!-- container for title + price + buttons -->
-              <div class="row justify-between col">
-                <!-- title + price -->
-                <div class="column justify-evenly">
-                    <!-- title -->
-                    <div
-                      class="text-h4 text-green text-weight-bold first_letter"
-                    >
-                      {{ item.title }}
-                    </div>
-                    <!-- price -->
-                    <div class="row items-baseline">
-                      <span
-                        class="text-white text-weight-bold text-h5 q-mr-xs"
-                      >
-                        &#3647&ensp;{{ item.price * item.quant }}
-                      </span>
-                      <span
-                        class="q-mr-xs text-white text-weight-bold text-h5"
-                      >/</span>
-                      <span class="text-grey text-weight-bold text-body1">
-                        &#3647&ensp;{{ item.price }}
-                      </span>
-                    </div>
+            <div class="q-mb-lg-md q-mb-xs-sm">
+              <div class="row items-center justify-between q-mb-lg-md q-mb-xs-xs">
+                <div class="text-h1 text-left text-white text-uppercase col-10  text-weight-bold">
+                  {{ $t('order') }}
                 </div>
 
-                <!-- buttons -->
-                <div class="column justify-between items-end">
-                  <q-btn flat round @click="cartStore.removeFromCart(item.id)">
-                    <q-icon name="img:/bin.svg" size="2.5rem" />
+                <!-- close + bin -->
+                <div class="row absolute cart_buttons">
+                  <q-btn flat round @click="cartStore.clearCart()" class="q-mr-sm">
+                    <q-icon name="img:/bin.svg" size="2.5rem" class="text-grey" />
                   </q-btn>
+                  <IconButton
+                    icon="close"
+                    textColor="grey"
+                    :rounded="false"
+                    :round="true"
+                    :flat="true"
+                    size="xl"
+                    class="cart_close_button"
+                    @click="closeDrawerCart"
+                  />
+                </div>
+              </div>
+              <div class="bg-negative full-width" style="height: 0.1rem" />
+            </div>
 
-                  <div class="row justify-between items-center">
-                    <IconButton
-                      round
-                      iconStyle="text-grey-1"
-                      :icon="evaPlusOutline"
-                      @click="() => cartStore.increaseItemsCount(item)"
-                      class="q-pa-xs bg-white"
-                    />
-                    <div
-                      class='text-h4 text-white q-mx-lg-md q-mx-xs-sm q-my-none'
-                    >{{ item.quant }}</div>
-                    <IconButton
-                      round
-                      class="bg-white q-pa-xs"
-                      iconStyle="text-grey-1"
-                      :icon="evaMinusOutline"
-                      @click="() => cartStore.decreaseItemsCount(item)"
-                    />
+            <div v-if="!cartStore.cart.length" class="q-pa-lg-md q-pa-xs-sm text-center">
+              <div class="text-h2">{{ $t('empty_cart') }}</div>
+            </div>
+
+            <q-scroll-area class="fit">
+              <div class="row container_settings">
+
+                <!-- flex row container -->
+                <div
+                  v-for="(item, index) in cartStore.cartExtended" :key="index"
+                  class="cart_product_item_v3 bg-grey-1 row pa-20"
+                >
+
+                  <!-- image -->
+                  <div class="col-3 q-mr-xl">
+                    <q-img
+                      :src="item.image"
+                      :ration="1"
+                      height="150px"
+                      fit="none"
+                      class="img_style_v3"
+                    >
+                      <template #loading>
+                        <div class="text-subtitle1 text-black">
+                          Loading...
+                        </div>
+                      </template>
+                    </q-img>
+                  </div>
+
+                  <!-- container for title + price + buttons -->
+                  <div class="row justify-between col">
+                    <!-- title + price -->
+                    <div class="column justify-evenly">
+                        <!-- title -->
+                        <div
+                          class="text-h3 text-green text-weight-bold first_letter"
+                        >
+                          {{ item.title }}
+                        </div>
+                        <!-- price -->
+                        <div class="row items-baseline">
+                          <span
+                            class="text-white text-weight-bold text-h2 q-mr-xs"
+                          >
+                            &#3647&ensp;{{ item.price * item.quant }}
+                          </span>
+                          <span class="q-mr-xs text-white text-weight-bold text-h3">/</span>
+                          <span class="text-grey text-weight-bold text-h4">
+                            &#3647&ensp;{{ item.price }}
+                          </span>
+                        </div>
+                    </div>
+
+                    <!-- buttons -->
+                    <div class="column justify-between items-end">
+                      <q-btn flat round @click="cartStore.removeFromCart(item.id)">
+                        <q-icon name="img:/bin.svg" size="2.5rem" />
+                      </q-btn>
+
+                      <div class="row justify-between items-center">
+                        <IconButton
+                          round
+                          iconStyle="text-grey-1"
+                          :icon="evaPlusOutline"
+                          @click="() => cartStore.increaseItemsCount(item)"
+                          class="q-pa-xs bg-white"
+                        />
+                        <div
+                          class='text-h2 text-white q-mx-lg-md q-mx-xs-sm q-my-none'
+                        >{{ item.quant }}</div>
+                        <IconButton
+                          round
+                          class="bg-white q-pa-xs"
+                          iconStyle="text-grey-1"
+                          :icon="evaMinusOutline"
+                          @click="() => cartStore.decreaseItemsCount(item)"
+                        />
+                      </div>
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+            </q-scroll-area>
+
+            <div class="full-width total_style bg-grey-3" v-if="cartStore.cart.length">
+              <div
+                class="q-mb-sm"
+                :class="[cartStore.cart.length > 0 ? 'q-mb-lg-md q-mb-xs-sm' : 'q-mb-none row justify-between items-center']">
+
+                <DividerThin class="bg-white mb-20" />
+
+                <div
+                  class="text-white text-h2 q-px-xl no-margin row justify-between fit"
+                >
+                  <div>{{ $t('total') }}</div>
+                </div>
+
+                <div class="text-white row justify-between q-px-xl">
+                  <div
+                    class="text-h3 row text-weight-regular q-gutter-x-sm text-white items-center"
+                  >
+                    <span>{{ $t('order') }}</span>
+                    <span>{{ cartStore.totalQuantity }}</span>
+                    <span>{{ $t('pieces', { count: cartStore.totalQuantity }) }}</span>
+                  </div>
+                  <div class="text-h2">
+                    {{ cartStore.totalPrice }} &ensp;&#3647
                   </div>
                 </div>
+
               </div>
-
-            </div>
-          </div>
-        </q-scroll-area>
-
-        <div class="full-width total_style bg-grey-3" v-if="cartStore.cart.length">
-          <div
-            class="q-mb-sm"
-            :class="[cartStore.cart.length > 0 ? 'q-mb-lg-md q-mb-xs-sm' : 'q-mb-none row justify-between items-center']">
-
-            <DividerThin class="bg-white mb-20" />
-
-            <div
-              class="text-white text-h4 q-px-xl no-margin row justify-between fit"
-            >
-              <div>{{ $t('total') }}</div>
-            </div>
-
-            <div class="text-white row justify-between q-px-xl">
               <div
-                class="text-body1 row text-weight-regular q-gutter-x-sm text-white items-center"
+                class="full-width q-px-xl q-pb-xl"
+                v-show="cartStore.cart.length"
               >
-                <span>{{ $t('order') }}</span>
-                <span>{{ cartStore.totalQuantity }}</span>
-                <span>{{ $t('pieces', { count: cartStore.totalQuantity }) }}</span>
-              </div>
-              <div class="text-h4">
-                {{ cartStore.totalPrice }} &ensp;&#3647
+                <RectangularButton
+                  class="fit q-py-xs-sm"
+                  :name="$t('checkout')"
+                  :disable="isDisabled"
+                  @click="submitOrder"
+                  :rounded="false"
+                  color="white"
+                  textColor="grey-3"
+                  classTitle="text-h4 text-weight-bold"
+                />
               </div>
             </div>
-
-          </div>
-          <div
-            class="full-width q-px-xl q-pb-xl"
-            v-show="cartStore.cart.length"
-          >
-            <RectangularButton
-              class="fit q-py-xs-sm"
-              :name="$t('checkout')"
-              :disable="isDisabled"
-              @click="submitOrder"
-              :rounded="false"
-              color="white"
-              textColor="grey-3"
-              classTitle="text-h4 text-weight-bold"
-            />
           </div>
         </div>
-      </div>
+        <OrderCheck v-else-if="app.orderDialog == true" />
+      </transition>
     </div>
   </transition>
   <transition name="backdrop_animation">
     <div class="drawer_backdrop" v-if="props.isOpen" @click="emit('click')" />
   </transition>
 
-  <template>
-    <OrderCheck />
-  </template>
 </template>
 
 <style scoped>
@@ -319,6 +323,15 @@ import DividerThin from '../../dividers/divider-thin.vue';
 
   .backdrop_animation-leave-to,
   .backdrop_animation-enter-from {
+    opacity: 0;
+  }
+
+  .slide-cart-enter-active,
+  .slide-cart-leave-active {
+    transition: opacity 0.5s;
+  }
+  .slide-cart-enter-from,
+  .slide-cart-leave-to {
     opacity: 0;
   }
 </style>
