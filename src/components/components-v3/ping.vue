391c705e64488a388610b15c7dacbf6702bad88d
Pavel Trubin <agsagds@gmail.com>
2024-05-16 16:36:31 +0600

    feat: Dialog via ping component

--- src/components/components-v3/ping.vue
+++ src/components/components-v3/ping.vue
@@ -10,7 +10,7 @@ const router = useRouter();
 const app = useAppStore();
 const formattedTime = ref(moment().format("LT").slice(0, -3));
 let healthCheckInterval: NodeJS.Timeout;
-const isCatHealthy = ref(false);
+const isCatUnhealthy = ref(false);
 const isWsHealthy = ref(false);
 
 const textColorClass = ref('text-black'); // Начальное значение
@@ -31,7 +31,7 @@ router.beforeEach((to, from, next) => {
 onMounted(() => {
   healthCheckInterval = setInterval(async () => {
     formattedTime.value = moment().format("LT").slice(0, -3);
-    isCatHealthy.value  = await apiHealthCheck(config.cat_health_check_interval_ms);
+    isCatUnhealthy.value  = !(await apiHealthCheck(config.cat_health_check_interval_ms));
     isWsHealthy.value = !!ws?.OPEN;
   }, config.cat_health_check_interval_ms);
 });
@@ -47,9 +47,22 @@ onUnmounted(() => {
   >
     <div :class="textColorClass">{{ app.kioskState.name }}</div>
     <div :class="textColorClass">{{ formattedTime }}</div>
-    <div :class="isCatHealthy ? 'ping_cat_light' : 'ping_cat_light__not_signal'" />
+    <div :class="!isCatUnhealthy ? 'ping_cat_light' : 'ping_cat_light__not_signal'" />
     <div :class="isWsHealthy ? 'ping_tdp_light' : 'ping_tdp_light__not_signal'" />
   </div>
+  <q-dialog
+      v-model="isCatUnhealthy"
+      persistent
+      full-width
+      transition-show="slide-up"
+      transition-hide="slide-down"
+    >
+    <q-card>
+        <q-card-section>
+          <div class="text-h6">Network Unavailable</div>
+        </q-card-section>
+      </q-card>
+    </q-dialog>
 </template>
 
 <style scoped>
